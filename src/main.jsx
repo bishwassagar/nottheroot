f65e575e70ca5fdb58503727668d44e60aea26bf
bishwassagar <bishwas@tuta.io>
2023-07-31 02:39:38 +0530

    added about section

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/App.jsx'
-import About from './components/about/About.jsx'
+import About_Bishwas from './components/about/About_Bishwas.jsx'
+import About_Musharraf from './components/about/About_Musharraf.jsx'
 import Footer from './components/footer/Footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <About />
-    <About />
-    <About />
-    <About />
+    <About_Bishwas />
+    <About_Musharraf />
     <Footer />
   </React.StrictMode>,
 )
