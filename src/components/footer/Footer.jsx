ed5c2b87ce970a4512cd9fa960e8e9cecca30a27
bishwassagar <bishwas@tuta.io>
2023-07-31 02:22:33 +0530

    updated hyperlinks

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="https://nottheroot.in/" className="hover:underline">
             nottheroot | CySec
           </a>
         </span>
